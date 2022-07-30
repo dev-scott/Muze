a9a597191d984f824cd2b38bd3b0a8c02a85a3ff
dev-scott <95773992+dev-scott@users.noreply.github.com>
2022-07-30 11:39:19 +0200

    muze

---
+++ js/toggle.js
@@ -0,0 +1,48 @@
+    const body = document.querySelector("body");
+    const navbar = document.querySelector(".navbar");
+    const menu = document.querySelector(".menu-list");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = ()=>{
+      menu.classList.add("active");
+      menuBtn.classList.add("hide");
+      cancelBtn.classList.add("show");
+      body.classList.add("disabledScroll");
+    }
+
+    cancelBtn.onclick = ()=>{
+      menu.classList.remove("active");
+      menuBtn.classList.remove("hide");
+      cancelBtn.classList.remove("show");
+      body.classList.remove("disabledScroll");
+    }
+
+    window.onscroll = ()=>{
+      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
+
+
+    // for galerie photo
+
+    let allGridItems = [...document.getElementsByClassName("grid-item")];
+let popupBg = document.getElementById("popup-bg");
+let popupImg = document.getElementById("popup-img");
+
+const openPopup = (e) => {
+  let gridItemClicked = e.target.closest(".grid-item");
+  console.log(gridItemClicked);
+  let clickedImageName = gridItemClicked.id;
+  popupBg.classList.add("active");
+  popupImg.src = `./images/${clickedImageName}.jpg`;
+};
+
+const closePopup = () => {
+
+  popupBg.classList.remove("active");
+
+};
+
+allGridItems.forEach((el) => el.addEventListener("click", openPopup));
+
+popupImg.addEventListener("click", (e) => e.stopPropagation());
+popupBg.addEventListener("click", closePopup);
